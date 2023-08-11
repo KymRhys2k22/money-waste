6d032c142ccdda750f8d24bab9f542e65499c224
Kym Rhys Mallari <107522146+KymRhys2k22@users.noreply.github.com>
2023-08-12 03:29:16 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import {
   Footer,
   Header,
   Main,
-<<<<<<< HEAD
-
 } from './Component'
 import useScrollPosition from './utils/useScrollPosition'
-=======
-  Nav
-} from './Component'
->>>>>>> 25f4eda4964cf08e44777c2831c596af9a6b92f9
+
+
+
 
 const App = () => {
   const useScroll = useScrollPosition
