ca40827e4db70844919684c0cd3d76e141b94d49
KymRhys2k22 <kymrhys@gmail.com>
2023-10-26 22:43:03 +0800

    add:animation messenger logo

--- src/Component/Messenger.jsx
+++ src/Component/Messenger.jsx
@@ -10,7 +10,7 @@ const Messenger = () => {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 1 }}
             transition={{ delay: 5 }}
-            className='fixed max-sm:right-3  bottom-5 z-50 right-5'>
+            className='fixed max-sm:right-3 hover:animate-pulse  bottom-5 z-50 right-5'>
             <a href='https://www.m.me/100076257069294' target="_blank">
                 <img src={messenger} alt="messenger" width={60} />
             </a >
